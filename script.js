f17bd2aa2104c576007b088882ff5bde0c5cbeba
Unknown <VSanchez899@west-mec.org>
2018-08-29 15:30:58 -0700

    changed

--- script.js
+++ script.js
@@ -12,44 +12,52 @@
 // global variables
 var selectedCity = "Tucson, AZ";
 var weatherReport;
+var httpRequest = false; // have an XHR object to use?
+//function to instantiate XHR
+function getRequestObject() {
+    alert("getRequestObject()");
+}
 //get weather called on load or button click to select city for default city 
 //or button click to selected city
 function getWeather(evt) {
-   var latitude;
-   var longitude;
-   if (evt.type !== "load") {
-      if (evt.target) {
-         selectedCity = evt.target.innerHTML;
-      } else if (evt.srcElement) {
-         selectedCity = evt.srcElement.innerHTML;
-      }
-   }
-   if (selectedCity === "Tucson, AZ") {
-      latitude = 37.7577;
-      longitude = -122.4376;
-   } else if (selectedCity === "Chicago, IL") {
-      latitude = 41.8337329;
-      longitude = -87.7321555;
-   } else if (selectedCity === "Montreal, QC") {
-      latitude = 45.5601062;
-      longitude = -73.7120832;
-   }
+    var latitude;
+    var longitude;
+    if (evt.type !== "load") {
+        if (evt.target) {
+            selectedCity = evt.target.innerHTML;
+        } else if (evt.srcElement) {
+            selectedCity = evt.srcElement.innerHTML;
+        }
+    }
+    if (selectedCity === "Tucson, AZ") {
+        latitude = 37.7577;
+        longitude = -122.4376;
+    } else if (selectedCity === "Chicago, IL") {
+        latitude = 41.8337329;
+        longitude = -87.7321555;
+    } else if (selectedCity === "Montreal, QC") {
+        latitude = 45.5601062;
+        longitude = -73.7120832;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
 }
 
 //retrive location cities form the page
 var locations = document.querySelectorAll("section ul li");
 //add click event listers to li cities
 for (var i = 0; i < locations.length; i++) {
-   if (locations[i].addEventListener) {
-      locations[i].addEventListener("click", getWeather, false);
-   } else if (locations[i].attachEvent) {
-      locations[i].attachEvent("onclick", getWeather);
-   }
+    if (locations[i].addEventListener) {
+        locations[i].addEventListener("click", getWeather, false);
+    } else if (locations[i].attachEvent) {
+        locations[i].attachEvent("onclick", getWeather);
+    }
 }
 
 //event listener on load called getWeather()
 if (window.addEventListener) {
-   window.addEventListener("load", getWeather, false);
+    window.addEventListener("load", getWeather, false);
 } else if (window.attachEvent) {
-   window.attachEvent("onload", getWeather);
+    window.attachEvent("onload", getWeather);
 }
--- solar.php
+++ solar.php
@@ -1,5 +1,5 @@
 <?php
-$WeatherSource = "https://api.forecast.io/forecast/apikey/" . $_GET["lat"] . "," . $_GET["lng"];
+$WeatherSource = "https://api.forecast.io/forecast/06d8ccc4b39b948ec1c19fc7f47738aa/" . $_GET["lat"] . "," . $_GET["lng"];
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
 readfile($WeatherSource);
